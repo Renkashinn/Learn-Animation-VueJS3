8a146088a01d21f4983cd34a153580511e5a1b8e
Failasuf Azka <failsfzka@gmail.com>
2024-12-28 12:10:41 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Molestias at nulla reprehenderit soluta accusantium
+      exercitationem voluptatibus fugit laudantium ab natus. Autem quam voluptate ducimus repudiandae quis incidunt
+      nostrum odio maiores!</p>
+  </div>
+</template>
+
+<style>
+.about {
+  max-width: 300px;
+  margin: 0 auto;
+}
+</style>
